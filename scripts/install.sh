142fcf92d33bfd28a608cf85d31e822071f87aad
vikash <vikash.choudhary@incaendo.com>
2025-06-23 16:50:58 +0530

    fix: working scripts with nvm loaded

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Load NVM
+# Load NVM for node and npm
 export NVM_DIR="/home/ec2-user/.nvm"
 source "$NVM_DIR/nvm.sh"
 nvm use 24
 
-# Run npm install
+# Go to app directory and install
 cd /home/ec2-user/my-node-app
 npm install
