44744c4efcb4da29b6b59b41b27fc62494fddaff
vikash <vikash.choudhary@incaendo.com>
2025-06-23 16:47:17 +0530

    fix: added nvm loading for npm/node access

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Load NVM
+export NVM_DIR="/home/ec2-user/.nvm"
+source "$NVM_DIR/nvm.sh"
+nvm use 24
+
+# Run npm install
 cd /home/ec2-user/my-node-app
 npm install
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# Load NVM
+export NVM_DIR="/home/ec2-user/.nvm"
+source "$NVM_DIR/nvm.sh"
+nvm use 24
+
 cd /home/ec2-user/my-node-app
 nohup npm start > app.log 2>&1 &
